4f5c923a4f28cc82ac4efae78ae5d61d2cbee075
Dennis Adrian <adriangbetancur@gmail.com>
2020-10-02 11:03:39 -0400

    Updates

---
+++ precache-manifest.8fa98ed3f67645a18189cb0333ddb57d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c5c30c56958e11390b781ba4835e45",
+    "url": "/cientifico-react/index.html"
+  },
+  {
+    "revision": "85ba806673ec83010c8f",
+    "url": "/cientifico-react/static/css/main.473df9ba.chunk.css"
+  },
+  {
+    "revision": "0f9d8e80597169c9acef",
+    "url": "/cientifico-react/static/js/2.d191c77d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cientifico-react/static/js/2.d191c77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ba806673ec83010c8f",
+    "url": "/cientifico-react/static/js/main.ace5ce06.chunk.js"
+  },
+  {
+    "revision": "0433255954babb232a7b",
+    "url": "/cientifico-react/static/js/runtime-main.e5ec1b39.js"
+  }
+]);
